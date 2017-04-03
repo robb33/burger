1e5d56b76c71c9ffa91d563209704e5674da0cf1
rob bee <rbenny@earthlink.net>
2017-04-03 12:01:43 -0400

    burger.js file & favicon

--- models/burger.js
+++ models/burger.js
@@ -1,34 +1,24 @@
-var express = require("express");
-var router = express.Router();
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-var burger = require('../models/burger.js');
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-
-router.get('/', function(req, res) {
-
-
-
-});
-
-
-
-router.post("/create", function(req, res){
-
-
-});
-
-
-
-
-
-router.put("/:id", function(req, res) {
-
-
-});
-
-
-
-
-module.exports = router;
-
-
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
